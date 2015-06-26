593c2bd785b9c7f350a5a557d8c54bf2ceca84dd
Dan Gwozdz <dev.dangw@gmail.com>
2015-06-26 15:53:01 -0700

    broke the less/css code into a separate css file and updated the fonts

--- examples/reactly-boilerplate/webpack.config.js
+++ examples/reactly-boilerplate/webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require("webpack");
 var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: {
@@ -30,11 +31,11 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader'
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
             },
             {
                 test: /\.less$/,
-                loader: 'style-loader!css-loader!less-loader'
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
             }
         ]
     },
@@ -42,6 +43,7 @@ module.exports = {
         extensions: ['', '.js', '.jsx']
     },
     plugins: [
+        new ExtractTextPlugin("[name].css"),
         new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.js")
     ]
 };
